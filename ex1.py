b62a4bee1bc01e2f6f03c892fa15df8a94ce6f2c
srinandhini2142 <srinandhini2142@gmail.com>
2026-07-10 10:45:27 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+'''from math import*
+a,b,c=eval(input('enter a,b,c values: '))
+s=sqrt(b*b-4*a*c)/2*a
+print(s)'''
+'''from math import*
+x2,x1=eval(input('enter x2,x1 values: '))
+distance=sqrt(pow(x2-x1,2))
+print(distance)'''
+from math import*
+s,a,b,c=eval(input('enter s,a,b,c values:'))
+A=sqrt((s*(s-a)(s-b)(s-c)))
+print(A)       
+       
